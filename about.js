6f075ed16bac8405f2871b9c21d09df8dad1039e
Tony Chen <randomperson130@hotmail.com>
2017-08-14 20:33:10 -0400

    blog added

--- about.js
+++ about.js
@@ -1,21 +1,50 @@
-document.getElementById("aboutButton").addEventListener("click", fade);
-
 function hasClass(element, cls) {
     return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
 };
 
-function fade() {
+function buttonsReset() {
+  buttons = document.getElementsByTagName("navig");
+  for (i = 0; i < buttons.length; i++) {buttons[i].removeAttribute("class");};
+};
+
+function buttonOff(buttonId) {
+  button = document.getElementById(buttonId)
+  button.setAttribute("class", "waiting2");
+  setTimeout (function () {button.removeAttribute("class");}, 650)
+
+};
+
+function aboutToggle() {
   var containers = document.getElementsByTagName("section");
   if (hasClass(containers[0],"aboutActive") == false) {
+    buttonsReset();
     for (i = 0; i < containers.length; i++) {containers[i].setAttribute("class", "aboutActive")};
     document.getElementById("aboutButton").setAttribute("class", "waiting");
     setTimeout (function () {
-      document.getElementById("aboutButton").setAttribute("class","aboutActive");
+      document.getElementById("aboutButton").setAttribute("class","active");
 }, 650)
   } else {
     for (i = 0; i < containers.length; i++) {containers[i].removeAttribute("class")};
-    document.getElementById("aboutButton").setAttribute("class", "waiting2");
-    setTimeout (function () {document.getElementById("aboutButton").removeAttribute("class");}, 650)
+    buttonOff("aboutButton");
   }
 
+};
+
+function blogToggle() {
+  var containers = document.getElementsByTagName("section");
+  if (hasClass(containers[0],"blogActive") == false) {
+    buttonsReset();
+    for (i = 0; i < containers.length; i++) {containers[i].setAttribute("class", "blogActive")};
+    document.getElementById("blogButton").setAttribute("class", "waiting");
+    setTimeout (function () {
+      document.getElementById("blogButton").setAttribute("class","active");
+}, 650)
+  } else {
+    for (i = 0; i < containers.length; i++) {containers[i].removeAttribute("class")};
+    buttonOff("blogButton");
   }
+
+};
+
+document.getElementById("aboutButton").addEventListener("click", aboutToggle);
+document.getElementById("blogButton").addEventListener("click", blogToggle);
